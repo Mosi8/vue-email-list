243bc0572d689eeb0a2bba291f3083706f119415
Mosi8 <mauriziomosi8@gmail.com>
2022-01-25 16:07:01 +0100

    completo

---
+++ myscript.js
@@ -0,0 +1,21 @@
+let app = new Vue({
+    el : '#app',
+    data : {
+        email : [],
+    },
+    methods : {
+        genEmail : function () {
+            for (let i = 0; i < 10; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((response) =>{
+                this.email.push(response.data.response);
+                console.log(this.email);
+                }) 
+            }   
+        }
+    }
+}
+);
+
+
+
